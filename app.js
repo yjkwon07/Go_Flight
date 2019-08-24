eaf9fe4640f8abbf9c016361408e7f394a10e0a6
YoungJun <dudwns9073@nate.com>
2019-08-24 11:59:16 +0900

    init express

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const path = require('path');
+const session = require('express-session');
+const flash = require('flash');
+
+const app = express();
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+app.set('port', process.env.PORT || 8001);
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser('GoFlight'));
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: 'GoFlight',
+    cookie: {
+        httpOnly: true,
+        secure: false,
+    }
+}));
+app.use(flash());
+
+app.listen(app.get('port'), () => {
+    console.log(`${app.get('port')}번 포트에서 서버 실행중입니다.`);
+})
