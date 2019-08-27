7eff5bac0adbc6f0fdb4a402068ca41f48a6c608
YoungJun <dudwns9073@nate.com>
2019-08-27 12:42:09 +0900

    auth, post 라우터 장착 [UPDATE]

--- app.js
+++ app.js
@@ -8,19 +8,22 @@ const passport = require('passport');
 require('dotenv').config(); // process.env에 설정
 
 const indexRouter = require('./routes');
+const authRouter = require('./routes/auth');
+const postRouter = require('./routes/post');
 const { sequelize } = require('./models');
 const passportConfig = require('./passport');
 
-const app = express();
-sequelize.sync();
-passportConfig(passport);
+const app = express(); // get express
+
+sequelize.sync(); // config start
+passportConfig(passport); // config start 
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 app.set('port', process.env.PORT || 8001);
 
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/static',express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
@@ -33,23 +36,26 @@ app.use(session({
         secure: false,
     }
 }));
+// connect-flash 미들웨어는 cookie-parser와 express-session을 사용하므로 이들보다는 뒤에 위치해야 합니다.
 app.use(flash());
 // passport 설정 초기화
 app.use(passport.initialize());
-// express session이 session을 만들고 난 후
-// passport가 session을 사용하여 사용자 정보를 저장한다.
-app.use(paseport.session());
+// express session이 session을 만들고 난 후 passport가 session을 사용하여 사용자 정보를 저장한다.
+app.use(passport.session());
 app.use('/', indexRouter);
+app.use('/auth', authRouter);
+app.use('/post', postRouter);
 
 app.use((_req, _res, next) => {
     const err = new Error('Not Found');
-    err.staus = 404;
+    err.status = 404;
     next(err);
 });
-app.use( (err, req, res, _next ) =>{
+
+app.use( (err, _req, res, _next ) =>{
     res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-    res.staus(err.status || 505);
+    res.locals.error = app.get('env') === 'development' ? err : {};
+    res.status(err.status || 505);
     res.render('error');
 });
 
