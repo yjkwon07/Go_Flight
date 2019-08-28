b7e2842e8a2b90bb2f3725c8c1d4fe6f2586573e
YoungJun <dudwns9073@nate.com>
2019-08-28 13:10:32 +0900

    user라우터 장착 [UPDATE[

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ require('dotenv').config(); // process.env에 설정
 const indexRouter = require('./routes');
 const authRouter = require('./routes/auth');
 const postRouter = require('./routes/post');
+const userRouter = require('./routes/user');
 const { sequelize } = require('./models');
 const passportConfig = require('./passport');
 
 const app = express(); // get express
-
 sequelize.sync(); // config start
 passportConfig(passport); // config start 
 
@@ -24,6 +24,7 @@ app.set('port', process.env.PORT || 8001);
 
 app.use(morgan('dev'));
 app.use('/static',express.static(path.join(__dirname, 'public')));
+app.use('/img', express.static(path.join(__dirname, '/public/upload')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
@@ -34,7 +35,7 @@ app.use(session({
     cookie: {
         httpOnly: true,
         secure: false,
-    }
+    },
 }));
 // connect-flash 미들웨어는 cookie-parser와 express-session을 사용하므로 이들보다는 뒤에 위치해야 합니다.
 app.use(flash());
@@ -42,9 +43,11 @@ app.use(flash());
 app.use(passport.initialize());
 // express session이 session을 만들고 난 후 passport가 session을 사용하여 사용자 정보를 저장한다.
 app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/auth', authRouter);
 app.use('/post', postRouter);
+app.use('/user', userRouter);
 
 app.use((_req, _res, next) => {
     const err = new Error('Not Found');
@@ -52,7 +55,7 @@ app.use((_req, _res, next) => {
     next(err);
 });
 
-app.use( (err, _req, res, _next ) =>{
+app.use((err, _req, res, _next) =>{
     res.locals.message = err.message;
     res.locals.error = app.get('env') === 'development' ? err : {};
     res.status(err.status || 505);
