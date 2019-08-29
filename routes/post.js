5a85b832bc57512187273f2e4bae4e2c297ccaa8
YoungJun <dudwns9073@nate.com>
2019-08-29 10:53:38 +0900

    게시 글/ 이미지 업로드 및 해시태그 검색기능 [UPDATE]

--- routes/post.js
+++ routes/post.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const multer = require('multer');
+const mkdir = require('../public/js/publicDir');
 const path = require('path');
+
 const router = express.Router();
-const { Post, Hashtag } = require('../models');
+const { Post, Hashtag, User } = require('../models');
+const { isLoggedIn } =require('./middlewares');
+
+// Create folder path
+mkdir.mkdirp('./public/upload');
 
 // multipart/from-data는 express.json과 express.urlencoded가 해석을 못 한다.
 // 이미지를 업로드하려면 폼을 multipart/form-data로,
@@ -16,26 +22,26 @@ const upload = multer({
         // filename: 파일명
         // cb(에러, 결괏값) -> callback
         destination(_req, _file, cb) {
-            cb(null, 'upload/');
+            cb(null, 'public/upload/');
         },
-        filename(req, file, cb) {
+        filename(_req, file, cb) {
             const ext = path.extname(file.originalname);
             // (에러 , 설정 )
             // 파일명 중복 막기
-            cb(null, path.basename(file.originalname, ext) + new Date().valueOf + ext);
+            cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
         },
     }),
     limit: { fileSize: 5 * 1024 * 1024 },
 });
 /*
-    single: 이미지 하나(필드명)
+    single: 이미지 하나(필드명-> id 혹은 name)
     array: 이미지 여러 개(단일 필드)
     fields: 이미지 여러 개( 여러 필드)
     none: 이미지가 아니다.
 */
-router.post('/img', upload.single('img'), (req, res) => {
-    console.log(req.file);
-    res.json({ url: `/img/${req.file.fieldname}` });
+router.post('/img', isLoggedIn , upload.single('img'), (req, res) => {
+    // console.log(req.file);
+    res.json({ url: `/img/${req.file.filename}` });
 });
 
 /*
@@ -43,32 +49,54 @@ router.post('/img', upload.single('img'), (req, res) => {
     사진 주소를 올리므로 none을 쓴다.
 */
 const upload2 = multer();
-router.post('/', upload2.none(), async (req, res ,next) => {
+router.post('/', isLoggedIn ,upload2.none(), async (req, res, next) => {
     try {
         const post = await Post.create({
             content: req.body.content,
             img: req.body.url,
             userId: req.user.id,
         });
-        const hashtags = req.body.content.match(/#[^\s]*/g);
-        if(hashtags){
+        const hashtags = req.body.content.match(/#[^\s#]*/g);
+        if (hashtags) {
             // 안녕하세요 #노드 #익스프레스
-            // hashtags = ['#노드' , '#익스프레스'] ['#노드', '#atom']
+            // hashtags = ['#노드', '#익스프레스'], ['#노드', '#atom']
             // #노드가 중복
             // findOrCreate: DB에 있으면 찾고 없으면 새로 생성
-            await Promise.all(hashtags.map(tag => Hashtag.findOrCreate({
-                where: {title: tag.slice(1).toLowerCase() },
-            })));
             // 저장결과가 result에 담는다.
-            await post.addHashtags(result.map(r=>r[0]));
-            /*
-                A.getB: 관계있는 로우 조회
-                A.addB: 관계 생성
-                A.setB: 관계 수정
-                A.removeB: 관계 제거
-            */
-           res.redirect('/');
+           const result =  await Promise.all(hashtags.map(tag => Hashtag.findOrCreate({
+                where: { title: tag.slice(1).toLowerCase() },
+            })));
+            await post.addHashtags(result.map(r => r[0])); // map으로 이차원 배열을 1차원으로 만들어주어 리턴한다.
+            res.redirect('/');
         }
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
+
+/*
+    A.getB: 관계있는 로우 조회
+    A.addB: 관계 생성
+    A.setB: 관계 수정
+    A.removeB: 관계 제거
+*/
+router.get('/hashtag', async (req, res, next) => {
+    const query = req.query.hashtag;
+    if(!query) {
+        return res.redirect('/');
+    }
+    try {
+        const hashtag = await Hashtag.findOne({ where : { title : query } });
+        let posts = [];
+        if(hashtag) {
+            posts = await hashtag.getPosts({ include: [{ model: User}] });
+        }
+        return res.render('main', {
+            title : `${query} | GoFlight`,
+            user: req.user,
+            twits: posts,
+        });
     } catch (error) {
         console.error(error);
         next(error);
