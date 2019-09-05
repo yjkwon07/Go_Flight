a0a824336b50f36c06a67b838c6cd86ef20bca59
YoungJun <dudwns9073@nate.com>
2019-09-05 16:25:15 +0900

    (Models) comments delete&& as, foreignKey [UPDATE]

--- models/index.js
+++ models/index.js
@@ -11,46 +11,16 @@ db.User = require('./user')(sequelize, Sequelize);
 db.Post = require('./post')(sequelize, Sequelize);
 db.Hashtag = require('./hashtag')(sequelize, Sequelize);
 
-// 1대1 관계 
-// 주가 되는 1이 먼저 나와야 한다.
-// db.User.hasOne(db.Post);
-// db.Post.belongsTo(db.User);
-
-// 1대다 관계
-// 1 (사용자 한명이 게시글을 많이 가지고 있다)
 db.User.hasMany(db.Post);
-// 다 (게시글은 사용자에 속해 있다)
-db.Post.belongsTo(db.User);
+db.Post.belongsTo(db.User, {as: "User", foreignKey: "userId"});
 
-// 다대다 관계는 belongsToMany(belongsTo가 아니다.)
-// through에는 새로 생기는 모델 이름을 넣어준다.(매칭 테이블)
-db.Post.belongsToMany(db.Hashtag, {through: 'PostHashtag'});
-db.Hashtag.belongsToMany(db.Post, {through: 'PostHashtag'});
-// 1. 안녕하세요. #노드 #익스프레스
-// 2. 안녕하세요. #노드 #제이드
-// 3. 안녕하세요 # 제이드 #퍼그
+db.Post.belongsToMany(db.Hashtag, {through: 'PostHashtag', as: "Hashtag", foreignKey: 'postId'});
+db.Hashtag.belongsToMany(db.Post, {through: 'PostHashtag', as: "Post", foreignKey: 'hashtagId'});
 
-// as: 매칭 모델 이름
-// foreignKey: 상대 테이블 아이디
-// A.belongsToMany(B, {as: 'Bname', foreignKey:'A_id'})
-db.User.belongsToMany(db.User, {through: 'Follow', as : 'Followers', foreignKey: 'followingId'});
-db.User.belongsToMany(db.User, {through: 'Follow', as : 'Followings', foreignKey: 'followerId'});
+db.User.belongsToMany(db.User, {through: 'Follow', as: 'Followers', foreignKey: 'followingId'});
+db.User.belongsToMany(db.User, {through: 'Follow', as: 'Followings', foreignKey: 'followerId'});
 
-// 1. 제로
-// 2. 네로
-// 3. 히어로
-
-// 1-2
-// 1-3
-// 2-3
-// 3-1
-// 1-4
-
-// 1. 제로
-// 2. 네로
-// 3. 히어로
-// 4. 바보
-db.User.belongsToMany(db.Post, {through: 'Like'});
-db.Post.belongsToMany(db.User, {through: 'Like', as:"Liker"});
+db.User.belongsToMany(db.Post, {through: 'Like', as: "Post", foreignKey: 'userId'});
+db.Post.belongsToMany(db.User, {through: 'Like', as: "Liker", foreignKey: 'postId'});
 
 module.exports = db;
