97cacba12d0880755eed9815fa81ca814dd5ce31
YoungJun <dudwns9073@nate.com>
2019-11-18 11:03:07 +0900

    app [UPDATE] - secure   - helmet, hpp 추가
    - logger
      - logger.js [CREATE] -> winston 추가
    
    - redis
      - redis., connect-redis 추가
      - server restart 후 session이 사라지는 이슈 해결
      - 서버 재시작하더라도 redisDB의 session 정보로 유지 할 수 있다.

--- app.js
+++ app.js
@@ -5,29 +5,46 @@ const path = require('path');
 const session = require('express-session');
 const flash = require('connect-flash');
 const passport = require('passport');
+const helmet = require('helmet');
+const hpp = require('hpp');
+const redis = require('redis');
+const RedisStore = require('connect-redis')(session);
 require('dotenv').config(); // process.env에 설정
 
 const indexRouter = require('./routes');
 const authRouter = require('./routes/auth');
 const postRouter = require('./routes/post');
 const userRouter = require('./routes/user');
+
 const { sequelize } = require('./models');
 const passportConfig = require('./passport');
+const logger = require('./logger');
 
 const app = express(); // get express
 sequelize.sync(); // config start
 passportConfig(passport); // config start 
 
+const redisClient = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOST);
+redisClient.auth(process.env.REDIS_PASSWORD, (err) => {
+    if(err) logger.error(err.message);
+});
+
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 app.set('port', process.env.PORT || 8001);
 
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'production') {
+    app.use(morgan('combined'));
+    app.use(helmet());
+    app.use(hpp());
+} else {
+    app.use(morgan('dev'));
+}
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(session({
+const sessionOption = {
     resave: false,
     saveUninitialized: false,
     secret: process.env.COOKIE_SECRET,
@@ -35,7 +52,20 @@ app.use(session({
         httpOnly: true,
         secure: false,
     },
-}));
+    store: new RedisStore({
+        client: redisClient,
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT,
+        pass: process.env.REDIS_PASSWORD,
+        logErrors: true,
+    }),
+};
+if (process.env.NODE_ENV === 'production') {
+    sessionOption.proxy = true;
+    // sessionOption.cookie.secure = true; // https
+}
+
+app.use(session(sessionOption));
 // connect-flash 미들웨어는 cookie-parser와 express-session을 사용하므로 이들보다는 뒤에 위치해야 합니다.
 app.use(flash());
 // passport 설정 초기화
@@ -51,10 +81,12 @@ app.use('/user', userRouter);
 app.use((_req, _res, next) => {
     const err = new Error('Not Found');
     err.status = 404;
+    logger.info('hello'); // console.info 대체 
+    logger.error(err.message); // console.error 대체 
     next(err);
 });
 
-app.use((err, _req, res, _next) =>{
+app.use((err, _req, res, _next) => {
     res.locals.message = err.message;
     res.locals.error = app.get('env') === 'development' ? err : {};
     res.status(err.status || 505);
